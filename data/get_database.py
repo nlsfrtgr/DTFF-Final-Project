d984fabf86bb8e0d642b2e784a9b8cbba072a99c
ageiller <geillerantoiner@gmail.com>
2021-12-13 21:06:34 +0100

    My data

---
+++ data/get_database.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 13 21:03:23 2021
+
+@author: Antoine
+"""
+
+
+
+pip install yfinance
+
+import yfinance as yf
+import pandas as pd
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import os
+
+def get_database (): 
+
+    list_tickers=['AAPL','INTC','F','PFE','T']
+    tickers = yf.Tickers(list_tickers)
+
+    end_data = date.today()
+
+    start_data = date.today() - relativedelta(years=15)
+
+    df = pd.DataFrame(tickers.download( start=start_data, end=end_data))
+    
+    data = df["Close"]
+    
+    input_direct = os.getcwd()
+    output_direct = "/data/stock_prices.csv"
+    res = input_direct+output_direct
+    
+    data.to_csv(res)
+    
+    return
